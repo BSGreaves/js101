a5bc1b1484ba702fdff6dcead4031caee0a1d6f3
Ben Greaves <bsgreaves@gmail.com>
2017-01-24 19:21:22 -0600

    Switch statement done

---
+++ day2.js
@@ -0,0 +1,14 @@
+//Switch Statements
+
+var day = "Friday";
+
+switch (day) {
+	case "Monday":
+		console.log("This blows");
+		break;
+	case "Friday":
+		console.log("Let's party");
+		break;
+	default:
+		console.log("The end of the world is nigh");
+}
