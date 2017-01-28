b96f27000a3d465e6a7cda802fe8e44eca1dc5c8
Ben Greaves <bsgreaves@gmail.com>
2017-01-28 10:57:59 -0600

    Added Objects

--- day3.js
+++ day3.js
@@ -1,1 +1,16 @@
-Placeholder
+//OBJECTS
+
+var myCat = {
+	tail: "long",
+	age: 29,
+	color: "blue",
+	friends: ["Ben", "Zoe", "Bobby"]
+}
+
+console.log("Age of cat: ", myCat.age);
+console.log("Color of cat: ", myCat["color"]);
+
+for(var myKey in myCat) {
+	console.log("myKey", myKey);
+	console.log("values", myCat[myKey]);
+}
